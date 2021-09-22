3c538421d7dfa7e68563ffbe5642d83b65552e81
zoietady <zoietady@gmail.com>
2021-09-22 22:23:42 +1000

    access update

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const data = require("./data.js");
 app.use(express.json());
 app.use(cookieParser());
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header("Access-Control-Allow-Credentials", true);
