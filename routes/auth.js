096c347a0da069d133d8a5366ecb15730c513125
zoietady <zoietady@gmail.com>
2021-09-22 22:49:33 +1000

    cookie samesite

--- routes/auth.js
+++ routes/auth.js
@@ -122,8 +122,8 @@ router.post('/login',async (req, res) => {
             refreshTokens.push(refreshToken);
 
             /* send json response containing token */
-            res.cookie("access_token", accessToken,{httpOnly: false})
-                .cookie("refresh_token", refreshToken, {httpOnly: false})
+            res.cookie("access_token", accessToken,{httpOnly: true, SameSite="None"})
+                .cookie("refresh_token", refreshToken, {httpOnly: true, SameSite="None"})
                 .json({ user_id: user.user_id,admin: user.admin, expires_in: "15min" })
                 .status(200);
 
