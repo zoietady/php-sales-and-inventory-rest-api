bb44064c289638c17d49d8c0a710cb7b9c3b167f
zoietady <zoietady@gmail.com>
2021-09-22 23:15:35 +1000

    access

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const data = require("./data.js");
 
 app.use(function (req, res, next) {
 
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
