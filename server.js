6ae5501842e77cbcaaeafe7faf85600eb2381f14
zoietady <zoietady@gmail.com>
2021-09-22 23:35:18 +1000

    access

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.options("/*", function(req, res, next){
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Credentials', true);
+
     res.send(200);
 });
 
