2362e2367b04297925176af2a10ce49eebf648e7
zoietady <zoietady@gmail.com>
2021-09-22 23:19:06 +1000

    access

--- server.js
+++ server.js
@@ -10,17 +10,14 @@ const cookieParser = require("cookie-parser");
 
 const data = require("./data.js");
 
-app.use(function (req, res, next) {
-
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    next();
-});
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
 const authRouter = require('./routes/auth');
