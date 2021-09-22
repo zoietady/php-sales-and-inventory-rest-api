b7f9c55ab9e5e1b403e61e33768411de8cab097d
zoietady <zoietady@gmail.com>
2021-09-22 23:13:06 +1000

    access

--- server.js
+++ server.js
@@ -10,14 +10,15 @@ const cookieParser = require("cookie-parser");
 
 const data = require("./data.js");
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header("Access-Control-Allow-Credentials", true);
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
     next();
 });
-
 app.use(express.json());
 app.use(cookieParser());
 
