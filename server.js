c4a477c935f9128ddb9d2d300cc14f8d05a3404c
zoietady <zoietady@gmail.com>
2021-09-22 23:27:14 +1000

    options route

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ const data = require("./data.js");
 app.use(express.json());
 app.use(cookieParser());
 
+app.options("/*", function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.send(200);
+});
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
