454475de901323e52ddd84c89031c7151c43525e
zoietady <zoietady@gmail.com>
2021-09-22 23:40:00 +1000

    access

--- server.js
+++ server.js
@@ -23,6 +23,14 @@ app.options("/*", function(req, res, next){
 });
 
 app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
